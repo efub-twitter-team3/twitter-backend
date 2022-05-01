2312b2763fd48d3c1988747524952df4b7da0385
MINJU-KIMmm <0112km@ewhain.net>
2022-05-01 17:03:13 +0900

    Add : identifier column 추가

--- src/main/java/com/efub/efubtwitterteam3/domain/User.java
+++ src/main/java/com/efub/efubtwitterteam3/domain/User.java
@@ -1,7 +1,10 @@
 package com.efub.efubtwitterteam3.domain;
 
+import lombok.Getter;
+
 import javax.persistence.*;
 
+@Getter
 @Entity
 @Table(name = "user")
 public class User {
@@ -12,6 +15,10 @@ public class User {
     @Column(length = 50, nullable = false)
     private String nickname;
 
+    @Column(length = 50, nullable = false)
+    private String identifier;
+
     @Column(length = 255, nullable = true)
     private String bio;
+
 }
