5d17c686ab0bb2bbf9ded787b88b1714b2766cd0
MINJU-KIMmm <0112km@ewhain.net>
2022-05-02 14:32:06 +0900

    Chore : 사용하지 않는 import 삭제

--- src/main/java/com/efub/efubtwitterteam3/EfubTwitterTeam3Application.java
+++ src/main/java/com/efub/efubtwitterteam3/EfubTwitterTeam3Application.java
@@ -2,7 +2,6 @@ package com.efub.efubtwitterteam3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class EfubTwitterTeam3Application {
